aa1e3c3313b350ac6122d3e247ff507eb462da7a
konpeki <92644639+konpeki02@users.noreply.github.com>
2022-04-11 15:20:50 -0500

    updated .gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include <algorithm>
 using namespace std;
 
-char board[10] = { '0','1', '2', '3', '4', '5', '6', '7', '8', '9' };
+char board[10] = { 'O','1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
 void display();
 int player_function();
